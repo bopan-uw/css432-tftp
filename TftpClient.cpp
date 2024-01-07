eb5a8b46c9933a4baf905c7d2665363c9ed3c931
bopan-uw <91004152+bopan-uw@users.noreply.github.com>
2024-01-06 20:58:33 -0800

    Clean up code and style (#23)

--- TftpClient.cpp
+++ TftpClient.cpp
@@ -4,101 +4,90 @@
 
 //Sample echo client program - CSS 432 - Winter 2024
 
-#include <stdio.h>
+#include <cstdio>
+#include <iostream>
 #include <fstream>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <stdlib.h>
-#include <errno.h>          // for retrieving the error number.
-#include <string.h>         // for strerror function.
-#include <signal.h>         // for the signal handler registration.
+#include <cstdlib>
+#include <cstring>
 #include <unistd.h>
 
 
-
-#define SERV_UDP_PORT 61125 //REPLACE WITH YOUR PORT NUMBER
-//#define SERV_HOST_ADDR "10.158.82.32" //REPLACE WITH SERVER IP ADDRESS
-#define SERV_HOST_ADDR "127.0.0.1" //REPLACE WITH SERVER IP ADDRESS
-
-
-/* A pointer to the name of this program for error reporting.      */
-
-char *progname;
+#define SERV_UDP_PORT 61125
+#define SERV_HOST_ADDR "127.0.0.1"
 
 /* Size of maximum message to send.                                */
+#define MAX_LINE 512
 
-#define MAXLINE 512
-
+/* A pointer to the name of this program for error reporting.      */
+char *program;
 
 /* The dg_cli function reads lines from the terminal, sends them   */
 /* to the echo server pointed to by pserv_addr, and prints to the  */
 /* terminal the echoed data. The local endpoint is sockfd. The     */
 /* function returns when an EOF is seen. Note that the server's    */
 /* address in pserv_addr is already initialized. Its size is held  */
-/* in servlen, so that the function works with other protocol      */
+/* in serv_len, so that the function works with other protocol     */
 /* families that have different address sizes.                     */
 
-void dg_cli(int sockfd, struct sockaddr *pserv_addr, int servlen)
-{
+void dg_cli(int sockfd, struct sockaddr *pserv_addr, int serv_len) {
 
 /* Various counter and buffer variables. The extra byte in the     */
 /* receive buffer is used to add a null to terminate the string,   */
 /* as the network routines do not use nulls but I/O functions do.  */
 
-    int     n;
-    char    sendline[MAXLINE], recvline[MAXLINE + 1];
+    size_t n;
+    char send_line[MAX_LINE], recv_line[MAX_LINE + 1];
 
 /* Main client loop. Terminates on EOF. Get terminal input on the  */
-/* sendline buffer (up to MAXLINE bytes).                          */
+/* send_line buffer (up to MAX_LINE bytes).                        */
 
-    while (fgets(sendline, MAXLINE, stdin) != NULL) {
+    while (fgets(send_line, MAX_LINE, stdin) != nullptr) {
 
 /* Find the string's length to pass it as a parameter to the send  */
 /* calls (excluding the null in the end).                          */
 
-        n = strlen(sendline);
+        n = strlen(send_line);
 
 /* Send data using the sockfd socket, to the server at pserv_addr. */
-/* We also pass the send buffer and its size (sendline and n), an  */
-/* unused flag byte (0) and the server's address size (servlen).   */
+/* We also pass the send buffer and its size (send_line and n), an */
+/* unused flag byte (0) and the server's address size (serv_len).  */
 /* The returned number is the number of bytes sent. If it is not n */
-/* either an error or an interrupt has occured.                    */
-        printf("Sending data to server: %s\n", sendline);
-        if (sendto(sockfd, sendline, n, 0, pserv_addr, servlen) != n)
-        {
-            printf("%s: sendto error on socket\n",progname);
+/* either an error or an interrupt has occurred.                   */
+        printf("Sending data to server: %s\n", send_line);
+        if (sendto(sockfd, send_line, n, 0, pserv_addr, serv_len) != n) {
+            perror("sendto error on socket.");
             exit(3);
         }
 
-/* Read the echoed data from the same socket into the recvline     */
-/* buffer (up to MAXLINE bytes). The server's address and its size */
+/* Read the echoed data from the same socket into the recv_line    */
+/* buffer (up to MAX_LINE bytes). The server's address and its size*/
 /* are returned, but we do not need them, so we use null pointers. */
 /* The return value is the number of bytes received.               */
 
-        printf("Waiting to receive data from server\n");
+        std::cout << "Waiting to receive data from server" << std::endl;
 
-        n = recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL);
-        if (n < 0)
-        {
-            printf("%s: recvfrom error\n",progname);
+        n = recvfrom(sockfd, recv_line, MAX_LINE, 0, nullptr, nullptr);
+        if (n < 0) {
+            perror("recvfrom error.");
             exit(4);
         } else {
-            printf("Received data from server: ");
-
+            std::cout << "Received data from server: " << std::endl;
 
 /* The exchanged data is not null terminated, as the string length */
 /* is explicitly sent. We need to null terminate the string before */
 /* using fputs to output it on the terminal.                       */
 
-            recvline[n] = 0;
-            fputs(recvline, stdout);
-            printf("\n************\n\n");
+            recv_line[n] = 0;
+            fputs(recv_line, stdout);
+            std::cout << "\n************\n" << std::endl;
 
+            // Writing received output to file. DO NOT CHANGE.
             std::ofstream output;
             output.open("output_client.txt", std::ios::app);
-            output << recvline;
+            output << recv_line;
             output.close();
         }
     }
@@ -108,40 +97,39 @@ void dg_cli(int sockfd, struct sockaddr *pserv_addr, int servlen)
 /* and the server's address and port (well-known numbers) before   */
 /* calling the dg_cli main loop.                                   */
 
-int main(int argc, char *argv[])
-{
-    int                     sockfd;
+int main(int argc, char *argv[]) {
+    program = argv[0];
+
+    int sockfd;
 
 /* We need to set up two addresses, one for the client and one for */
 /* the server.                                                     */
 
-    struct sockaddr_in      cli_addr, serv_addr;
-    progname = argv[0];
+    struct sockaddr_in cli_addr, serv_addr;
 
 /* Initialize first the server's data with the well-known numbers. */
 
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family      = AF_INET;
+    memset(&serv_addr, 0, sizeof(serv_addr));
 
-/* The system needs a 32 bit integer as an Internet address, so we */
-/* use inet_addr to convert the dotted decimal notation to it.     */
 
+/* Fill in serv_addr information */
+
+    serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(SERV_HOST_ADDR);
     serv_addr.sin_port        = htons(SERV_UDP_PORT);
 
 /* Create the socket for the client side.                          */
 
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-    {
-        printf("%s: can't open datagram socket\n",progname);
-        exit(1);
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("client socket creation failed.");
+        exit(EXIT_FAILURE);
     }
 
 /* Initialize the structure holding the local address data to      */
 /* bind to the socket.                                             */
 
-    bzero((char *) &cli_addr, sizeof(cli_addr));
-    cli_addr.sin_family      = AF_INET;
+    memset(&cli_addr, 0, sizeof(cli_addr));
+    cli_addr.sin_family = AF_INET;
 
 /* Let the system choose one of its addresses for you. You can     */
 /* use a fixed address as for the server.                          */
@@ -152,8 +140,7 @@ int main(int argc, char *argv[])
 /* well-known). Using 0 here lets the system allocate any free     */
 /* port to our program.                                            */
 
-
-    cli_addr.sin_port        = htons(0);
+    cli_addr.sin_port = htons(0);
 
 /* The initialized address structure can be now associated with    */
 /* the socket we created. Note that we use a different parameter   */
@@ -161,18 +148,17 @@ int main(int argc, char *argv[])
 /* this program can find out how it was terminated. The number is  */
 /* up to you, the only convention is that 0 means success.         */
 
-    if (bind(sockfd, (struct sockaddr *) &cli_addr, sizeof(cli_addr)) < 0)
-    {
-        printf("%s: can't bind local address\n",progname);
-        exit(2);
+    if (bind(sockfd, (struct sockaddr *) &cli_addr, sizeof(cli_addr)) < 0) {
+        perror("Can't bind local address.");
+        exit(EXIT_FAILURE);
     }
 
 /* Call the main client loop. We need to pass the socket to use    */
 /* on the local endpoint, and the server's data that we already    */
 /* set up, so that communication can start from the client.        */
-    printf("Bind socket complete\n");
+    std::cout << "Bind socket complete" << std::endl;
 
-    printf("Type a message to send to server...\n");
+    std::cout << "Type a message to send to server..." << std::endl;
     dg_cli(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
 
 /* We return here after the client sees the EOF and terminates.    */
