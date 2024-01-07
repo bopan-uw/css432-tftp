f1b7aea75e7e2e65680bf95526f60dc61e33b2ac
Boxiang Pan <bopan@uw.edu>
2024-01-06 17:14:00 -0800

    extract valgrind result

--- .github/workflows/check_memory_leak.sh
+++ .github/workflows/check_memory_leak.sh
@@ -19,4 +19,31 @@ valgrind --leak-check=full \
 kill "$(jobs -p)"
 
 cat valgrind-out-server.txt
+echo "Checking memory leak for server..."
+result1=$(grep -c "definitely lost: 0 bytes in 0 blocks" valgrind-out-server.txt)
+result2=$(grep -c "indirectly lost: 0 bytes in 0 blocks" valgrind-out-server.txt)
+if [[ $(grep -c "All heap blocks were freed -- no leaks are possible" valgrind-out-server.txt) -eq 1 ]]
+then
+  echo "Memory check passed. All heap blocks were freed"
+elif [[ $result1 -eq 1 && $result2 -eq 1 ]]
+then
+  echo "Memory check passed."
+else
+  echo "Detected memory leak!"
+  exit 1
+fi
+
 cat valgrind-out-client.txt
+echo "Checking memory leak for client..."
+result1=$(grep -c "definitely lost: 0 bytes in 0 blocks" valgrind-out-client.txt)
+result2=$(grep -c "indirectly lost: 0 bytes in 0 blocks" valgrind-out-client.txt)
+if [[ $(grep -c "All heap blocks were freed -- no leaks are possible" valgrind-out-client.txt) -eq 1 ]]
+then
+  echo "Memory check passed. All heap blocks were freed"
+elif [[ $result1 -eq 1 && $result2 -eq 1 ]]
+then
+  echo "Memory check passed."
+else
+  echo "Detected memory leak!"
+  exit 1
+fi
